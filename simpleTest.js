eebf445aab5e3a5eb2886266f3445e06ed1ad793
Snuffy <rodrigo.sgarabotto.scola@gmail.com>
2021-12-15 15:20:43 -0300

    search Algorithms done

--- simpleTest.js
+++ simpleTest.js
@@ -1,8 +1,47 @@
-const recursPushArr = arr => {
-	if (arr.length >= 5) {
-		return arr
+class QuestLine {
+	constructor() {
+		this.quests = new LinkedList()
 	}
-	arr.push(1)
-	return recursPushArr([...arr, arr])
 }
-console.log(recursPushArr([]))
+class LinkedList {
+	constructors() {
+		this.head = null
+		this.tail = null
+	}
+	addHead(data) {
+		const newHead = new Node(data)
+		const currentHead = this.head
+		this.head = newHead
+		if (currentHead) {
+			this.head.setNextNode(currentHead)
+		}
+	}
+	removeHead() {
+		const removedHead = this.head
+		if (!removedHead) return
+		this.head = removedHead.next
+		return this.head.data
+	}
+}
+class Node {
+	constructor(data) {
+		this.data = data
+		this.next = null
+	}
+	getNextNode() {
+		return this.next
+	}
+	setNextNode(node) {
+		if (node instanceof Node) {
+			return (this.next = node)
+		}
+		throw new Error("must be an instance of node")
+	}
+}
+const tower = new QuestLine()
+const quest = new LinkedList()
+
+for (let i = 0; i < 12; i++) {
+	quest.addHead(i)
+}
+console.log(quest)
