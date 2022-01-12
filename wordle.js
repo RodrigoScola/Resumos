6448685f669b5c2421c64d69a0d2b2aedef0ceda
Snuffy <rodrigo.sgarabotto.scola@gmail.com>
2022-01-12 18:20:39 -0300

    formik and chakra

--- wordle.js
+++ wordle.js
@@ -1,20 +1,65 @@
 const allWords = require("an-array-of-english-words")
-
 class Word {
-	constructor(length) {
-		this.wordLength = length
-		this.words = allWords
-		this.usedLetters = []
+	constructor(size) {
+		this.size = size
+		this.currentWords = allWords
 		this.checkLength()
 	}
-	log() {
-		console.log(this.words)
-	}
 	checkLength() {
-		this.words = this.words.filter(word => word.length == this.wordLength)
+		this.currentWords = this.currentWords.filter(word => {
+			return word.length == this.size
+		})
 	}
-	addLetter(letter, rightSpot = false, position = -1) {}
+	log() {
+		console.log(this.currentWords)
+		return this.currentWords
+	}
+	logAll() {
+		this.currentWords.map(value => {
+			console.log(value)
+		})
+	}
+	hasLetter(letter = "a") {
+		this.currentWords.map(value => {
+			if (value.includes(letter)) {
+				console.log(value)
+				return value
+			}
+			return value
+		})
+	}
+	filterOut(letter, num = -1) {
+		this.currentWords = this.currentWords.filter(word => {
+			return word.split("").indexOf(letter) == num
+		})
+	}
+	addLetter(letter, type = "gray", position) {
+		if (type == "gray") {
+			this.filterOut(letter)
+		} else if (type == "green") {
+			this.filterOut(letter, position)
+		} else if (type == "yellow") {
+			this.currentWords = this.currentWords.filter(word => {
+				return word.split("").indexOf(letter) !== position
+			})
+		}
+		// this.log()
+	}
 }
 const word = new Word(5)
-
-word.log()
+word.addLetter("m")
+word.addLetter("u")
+word.addLetter("s")
+word.addLetter("i")
+word.addLetter("c")
+word.addLetter("a", "yellow", 0)
+word.addLetter("g")
+word.addLetter("e")
+word.addLetter("n")
+word.addLetter("t")
+word.addLetter("f", "green", 0)
+word.addLetter("l")
+word.addLetter("o", "yellow", 2)
+word.addLetter("r", "yellow", 3)
+word.addLetter("a", "yellow", 4)
+word.logAll()
